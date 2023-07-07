ce0e0c759c05e12ef2fd93542b11c75693144e50
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-07-07 05:41:30 -0400

    Updating single post Page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import SignUP from './Components/SignUp';
 import Allpost from './Components/Posts/Allpost';
+import Post from './Components/Posts/Post';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<SignUP/>}/>
           <Route path='/allposts' element={<Allpost/>}/>
+          <Route path='/posts/:id' element={<Post/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Posts/Post.jsx
+++ src/Components/Posts/Post.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import './Post.css'
 
-const Post=()=>{
+const Post= () => {
+    const [post, setPost]= useState({});
+    const {id} = useParams();
+    const fetchData = async()=>{
+        const base_url = `https://blog-api-8337.onrender.com/${id}`
+        try {
+            await fetch(base_url)
+            .then((res)=> res.json())
+            .then(data => setPost(data))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    useEffect(()=>{
+        fetchData()
+    }, [])
+    
+    
     return(
-        <div>
-
+        <div className="singlepost">
+            {
+                <div className="post-content">
+                    <div style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+                        <h1>{post.title}</h1>
+                        <em>Views: {post.views}</em>
+                    </div>
+                    
+                    <p>{post.title}</p>
+                    <p>{post.summary}</p>
+                    <div dangerouslySetInnerHTML={{__html: post.content}}></div>
+                </div>
+            }
         </div>
     )
 }
